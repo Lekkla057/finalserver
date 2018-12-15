1eba0bd4a1c9359943d2564b2f32e2bf79557d01
lekkla <lekkla.tong@gamil.com>
2018-12-15 14:18:10 +0700

    connect

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')();
-var db = pgp('postgres://qzwdfalyaqhdjf:f202bed3f53d1dc03b523853492dbfc397e1208f83d2e775b45f46c9652fe5bd@ec2-54-243-147-162.compute-1.amazonaws.com:5432/d70ueqr41e79i9?ssl=true');
+var db = pgp('postgres://mqvvplpgwkwaww:bbe283a5d4ce86d696c0794bfca51f66630f8553225b83bff3880b169880250a@ec2-54-243-147-162.compute-1.amazonaws.com:5432/ddhq6akm6qbbtp?ssl=true');
 
 function getAllProducts(req, res) {
     db.any('select * from categories')
